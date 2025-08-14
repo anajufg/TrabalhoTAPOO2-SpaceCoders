340a6c805748ba81ea2c24edbe513e56f686f312
anajufg <anajufg127@gmail.com>
2025-08-14 09:01:37 -0300

    ...

--- Asteroides/Server/Server.cs
+++ Asteroides/Server/Server.cs
@@ -9,7 +9,7 @@ public class Server
 
     public async Task StartServer(int port)
     {
-        listener = new TcpListener(IPAddress.Any, port);
+        listener = new TcpListener(IPAddress.Parse("10.0.0.66"), port);
         listener.Start();
 
         Console.WriteLine($"Aguardando jogadores na porta {port}...");
@@ -24,6 +24,17 @@ public class Server
             }
             // teste
             Console.WriteLine($"Novo jogador conectado! Total: {players.Count}");
+            var msg = "hello";
+            await Send(newClient, msg);
+            // teste
+            try
+            {
+                Console.WriteLine(await Receive(newClient));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro no cliente: {ex.Message}");
+            }
         }
     }
 
@@ -45,8 +56,8 @@ public class Server
             Console.WriteLine($"Jogador desconectado. Restantes: {players.Count}");
             return null;
 
-        } 
-        else 
+        }
+        else
         {
             string msg = Encoding.ASCII.GetString(buffer, 0, len);
             // teste
@@ -61,4 +72,25 @@ public class Server
         var stream = client.GetStream();
         await stream.WriteAsync(data, 0, data.Length);
     }
+    
+    // public async Task SendAll(string msg)
+    // {
+    //     var data = Encoding.ASCII.GetBytes(msg);
+    //     lock (players)
+    //     {
+    //         foreach (var player in players.ToList())
+    //         {
+    //             try
+    //             {
+    //                 var stream = player.GetStream();
+    //                 await stream.WriteAsync(data, 0, data.Length);
+    //             }
+    //             // caso o player se desconecte
+    //             catch
+    //             {
+    //                 players.Remove(player);
+    //             }
+    //         }
+    //     }
+    // }
 }
--- Asteroides/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Asteroides/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8ae9e5a6f5ebbd99866ba7d4a2b1abfededd4d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdf50ffd57c60d90c2b46cbb3206658cc526c9f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
